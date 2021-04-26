9d6b62ffa64ced4e2aee308a541aa7b35d5a57fa
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-04-26 13:47:18 -0400

    shapeArea

--- codesignal_reset.py
+++ codesignal_reset.py
@@ -57,3 +57,16 @@ def adjacentElementsProduct(inputArray):
         if inputArray[i] * inputArray[i+1] > largest_product:
             largest_product = inputArray[i] * inputArray[i+1]
     return largest_product
+
+"""
+Below we will define an n-interesting polygon. Your task is to 
+find the area of a polygon for a given n.
+
+A 1-interesting polygon is just a square with a side of length 
+1. An n-interesting polygon is obtained by taking the n - 1-
+interesting polygon and appending 1-interesting polygons to its rim,
+ side by side. 
+"""
+
+def shapeArea(n):
+    return n*n + ((n-1)*(n-1))
