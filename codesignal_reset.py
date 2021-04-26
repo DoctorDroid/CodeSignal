5185f57cde0691efabc6173d4abd4db2ee0b54a6
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-04-26 13:57:50 -0400

    makeArrayConsecutive2

--- codesignal_reset.py
+++ codesignal_reset.py
@@ -70,3 +70,21 @@ interesting polygon and appending 1-interesting polygons to its rim,
 
 def shapeArea(n):
     return n*n + ((n-1)*(n-1))
+    
+"""
+Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+Example
+
+For statues = [6, 2, 3, 8], the output should be
+makeArrayConsecutive2(statues) = 3.
+
+Ratiorg needs statues of sizes 4, 5 and 7.
+"""
+def makeArrayConsecutive2(statues):
+    in_order = sorted(statues)
+    print(in_order)
+    count = 0
+    for i in range(len(in_order)-1):
+        count += in_order[i+1]-in_order[i]-1
+    return count
