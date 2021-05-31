ed08ff071d501a6ea495e9039a30a501e99134c2
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-05-31 11:00:00 -0400

    avoidObstacles

--- CodeSignal Intro 17up.py
+++ CodeSignal Intro 17up.py
@@ -134,3 +134,30 @@ There is no first number.
             if int(l[i])>255:
                 return False
         return True
+
+
+""" 
+
+## avoidObstacles
+
+
+You are given an array of integers representing coordinates of obstacles situated on a straight line.
+
+Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer.
+
+Find the minimal length of the jump enough to avoid all the obstacles.
+
+Example
+
+For inputArray = [5, 3, 6, 7, 9], the output should be
+avoidObstacles(inputArray) = 4.
+"""
+def avoidObstacles(inputArray):
+    start = 0
+    increment = 1
+    while (start < max(inputArray)):
+        start += increment
+        if start in inputArray:
+            start = 0
+            increment += 1
+    return increment
