b407d8e370eae3f31d943f0c01533ed891a3aa43
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-06-01 21:47:03 -0400

    Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).

--- CodeSignal Intro 17up.py
+++ CodeSignal Intro 17up.py
@@ -307,3 +307,22 @@ chessBoardCellColor(cell1, cell2) = true.
  """   
 def chessBoardCellColor(cell1, cell2):
     return (ord(cell1[0]) + int(cell1[1]))%2 == (ord(cell2[0]) + int(cell2[1]))%2
+
+    """##circleOfNumbers
+
+    Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+
+Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+
+Example
+
+For n = 10 and firstNumber = 2, the output should be
+circleOfNumbers(n, firstNumber) = 7.
+    """
+
+def circleOfNumbers(n, firstNumber):
+    if firstNumber == n/2:
+        return 0
+    if firstNumber < n/2:
+        return firstNumber+ n/2
+    return firstNumber- n/2
