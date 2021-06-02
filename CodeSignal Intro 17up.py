35ea8a067b4a750dc824c831b41b7e33196891ee
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-06-01 21:33:21 -0400

    chessBoardCellColor

--- CodeSignal Intro 17up.py
+++ CodeSignal Intro 17up.py
@@ -295,3 +295,15 @@ def variableName(name):
         return False
     return True
     
+ """
+ ## chessBoardCellColor
+
+Given two cells on the standard chess board, determine whether they have the same color or not.
+
+Example
+
+For cell1 = "A1" and cell2 = "C3", the output should be
+chessBoardCellColor(cell1, cell2) = true.
+ """   
+def chessBoardCellColor(cell1, cell2):
+    return (ord(cell1[0]) + int(cell1[1]))%2 == (ord(cell2[0]) + int(cell2[1]))%2
