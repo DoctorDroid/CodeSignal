fbba9421ef2c1479b1703c75e62fd59498cec985
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-06-06 15:15:05 -0400

    absoluteValuesSumMinimization

--- CodeSignal Intro 17up.py
+++ CodeSignal Intro 17up.py
@@ -327,3 +327,64 @@ def circleOfNumbers(n, firstNumber):
     if firstNumber < n/2:
         return firstNumber+ n/2
     return firstNumber- n/2
+    """
+    ## depositProfit
+
+    You have deposited a specific amount of money into your bank account. Each year your balance increases at the same growth rate. With the assumption that you don't make any additional deposits, find out how long it would take for your balance to pass a specific threshold.
+
+Example
+
+For deposit = 100, rate = 20, and threshold = 170, the output should be
+depositProfit(deposit, rate, threshold) = 3.
+
+Each year the amount of money in your account increases by 20%. So throughout the years, your balance would be:
+
+year 0: 100;
+year 1: 120;
+year 2: 144;
+year 3: 172.8.
+Thus, it will take 3 years for your balance to pass the threshold, so the answer is 3.
+    """
+def depositProfit(deposit, rate, threshold):
+    count = 0
+    while deposit < threshold:
+        deposit += deposit * (rate/100)
+        count += 1
+    return count
+
+""" 
+## absoluteValuesSumMinimization
+
+Given a sorted array of integers a, your task is to determine which element of a is closest to all other values of a. In other words, find the element x in a, which minimizes the following sum:
+
+abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+(where abs denotes the absolute value)
+
+If there are several possible answers, output the smallest one.
+
+Example
+
+For a = [2, 4, 7], the output should be absoluteValuesSumMinimization(a) = 4.
+
+for x = 2, the value will be abs(2 - 2) + abs(4 - 2) + abs(7 - 2) = 7.
+for x = 4, the value will be abs(2 - 4) + abs(4 - 4) + abs(7 - 4) = 5.
+for x = 7, the value will be abs(2 - 7) + abs(4 - 7) + abs(7 - 7) = 8.
+The lowest possible value is when x = 4, so the answer is 4.
+
+For a = [2, 3], the output should be absoluteValuesSumMinimization(a) = 2.
+
+for x = 2, the value will be abs(2 - 2) + abs(3 - 2) = 1.
+for x = 3, the value will be abs(2 - 3) + abs(3 - 3) = 1.
+Because there is a tie, the smallest x between x = 2 and x = 3 is the answer.
+"""
+
+def absoluteValuesSumMinimization(a):
+    lst = []
+    sums = []
+    for x in a:
+        lst.append([abs(x - c) for c in a])
+    for i in range(len(lst)):
+        sums.append(sum(lst[i]))
+    for j in range(len(sums)):
+        if sums[j] == min(sums):
+            return a[j] 
