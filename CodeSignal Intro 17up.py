da2d7d9b1c690b7ef6b69f416266cd82792403d8
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-06-06 19:32:19 -0400

    extractEachKth

--- CodeSignal Intro 17up.py
+++ CodeSignal Intro 17up.py
@@ -398,3 +398,24 @@ def absoluteValuesSumMinimization(a):
     for x in a:
         sums.append(sum([abs(x - c) for c in a])) # list of sums 
     return a[sums.index(min(sums))]
+
+"""
+
+## extractEachKth
+
+Given array of integers, remove each kth element from it.
+
+Example
+
+For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3, the output should be
+extractEachKth(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].
+"""
+
+def extractEachKth(inputArray, k):
+    k = k-1
+    if k == 0:
+        return[]
+    for i in range(k,len(inputArray),k):
+        if i < len(inputArray):
+            del inputArray[i]
+    return inputArray
