821ec18f8e923e4247192ec1c11b1395dff58fc3
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-06-29 11:23:31 -0400

    digitDegree

--- CodeSignal Intro 17up.py
+++ CodeSignal Intro 17up.py
@@ -497,3 +497,27 @@ def growingPlant(upSpeed, downSpeed, desiredHeight):
         if h >= desiredHeight:
             return days
         h -= downSpeed
+
+""" 
+Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its digits until we get to a one digit number.
+
+Given an integer, find its digit degree.
+
+Example
+
+For n = 5, the output should be
+digitDegree(n) = 0;
+For n = 100, the output should be
+digitDegree(n) = 1.
+1 + 0 + 0 = 1.
+For n = 91, the output should be
+digitDegree(n) = 2.
+9 + 1 = 10 -> 1 + 0 = 1.
+"""
+
+def digitDegree(n):
+    count = 0
+    while n > 9:
+        n = sum([int(digit) for digit in (str(n))])
+        count += 1
+    return count
