48aa3f76662974bb1716f2c78f1bbbdca0c40780
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-06-29 14:51:41 -0400

    differentSquares

--- CodeSignal Intro 17up.py
+++ CodeSignal Intro 17up.py
@@ -591,3 +591,44 @@ passes all sample and 19/20 hidden
 """
 passes all sample, no hidden
 """
+
+"""
+Given a rectangular matrix containing only digits, calculate the number of different 2 × 2 squares in it.
+
+Example
+
+For
+
+matrix = [[1, 2, 1],
+          [2, 2, 2],
+          [2, 2, 2],
+          [1, 2, 3],
+          [2, 2, 1]]
+the output should be
+differentSquares(matrix) = 6.
+
+Here are all 6 different 2 × 2 squares:
+
+1 2
+2 2
+2 1
+2 2
+2 2
+2 2
+2 2
+1 2
+2 2
+2 3
+2 3
+2 1
+
+
+2 are identical therfore are not counted.
+"""
+
+def differentSquares(matrix):
+    set_ms = set()
+    for i in range(len(matrix)-1):
+        for j in range(len(matrix[0])-1):
+            set_ms.add((matrix[i][j], matrix[i+1][j],matrix[i][j+1],matrix[i+1][j+1]))
+    return len(set_ms)
