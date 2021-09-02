d62924ac45a64241c146140174a58c7af030653c
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-09-02 08:49:12 -0400

    mostFrequentDigitSum

---
+++ mostFrequentDigitSum.py
@@ -0,0 +1,32 @@
+# Function to get sum of digits 
+def getSum(n):
+    sum = 0
+    while (n != 0):
+        sum += (n % 10) # remainder 
+        n = n//10       # chops off last digit
+    return sum
+
+def mostFrequentDigitSum(n):
+    steps = [n] #begin steps list with n
+    sums_list = [] #intitialize all other vars to empty or 0
+    counts = {}
+    max_count = 0
+    max_sum = 0
+    while n != 0: #build step list
+        steps.append(n - getSum(n))
+        n = steps[-1]
+    for each in steps:
+        sums_list.append(getSum(each)) #build sum list
+    for each in sums_list:
+        if each in counts: #count sums
+            counts[each] += 1
+        else:
+            counts[each] = 0
+    for key in counts: #find highest count(s)
+        if counts[key] > max_count:
+            max_count = counts[key]
+    for i in range(len(steps)): 
+        if counts[sums_list[i]] == max_count: #search for highest counts
+            if sums_list[i] > max_sum:
+                max_sum = sums_list[i] #assign highest sum value to return
+    return max_sum
