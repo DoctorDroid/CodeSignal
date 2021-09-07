c7b5daf6c5388c86dbb5b881d935f8459c06b55e
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-09-07 15:57:59 -0400

    switchLights2

--- switchLights.py
+++ switchLights.py
@@ -8,3 +8,11 @@ def switchLights(a):
         a[i] %= 2
         # print(a)
     return a
+
+def switchLights(a):
+    flips = 0
+    for i in range(len(a)-1, -1, -1):
+        if a[i]:
+            flips += 1
+        a[i] = (a[i] + flips) % 2
+    return a
